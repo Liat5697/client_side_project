992ad71a67d9b1a9b4161ffc5e92fec5de19bcf3
Liat5697 <liat.a5697@gmail.com>
2024-03-27 20:55:54 +0200

    liat

--- main.js
+++ main.js
@@ -2,3 +2,18 @@ const container = document.getElementById('container');
 const imagesArray = [{ src: '/images/instagram.svg', isDisplay: false, imageType: 1 },
                     { src: '/images/instagram.svg', isDisplay: false, imageType: 1 },
                     ]
+                    [{ src: '/images/facebook.svg', isDisplay: false, imageType: 1 },
+                    { src: '/images/facebook.svg', isDisplay: false, imageType: 1 },
+                    ]
+                    [{ src: '/images/messenger.svg', isDisplay: false, imageType: 2 },
+                    { src: '/images/messenger.svg', isDisplay: false, imageType: 2 },
+                    ]
+                    [{ src: '/images/tiktok.svg', isDisplay: false, imageType: 3 },
+                    { src: '/images/tiktok.svg', isDisplay: false, imageType: 3 },
+                    ]
+                    [{ src: '/images/whatsapp.svg', isDisplay: false, imageType: 4 },
+                    { src: '/images/whatsapp.svg', isDisplay: false, imageType: 4 },
+                    ]
+                    [{ src: '/images/youtube.svg', isDisplay: false, imageType: 5 },
+                    { src: '/images/youtube.svg', isDisplay: false, imageType: 5 },
+                    ]
