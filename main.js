f344b894bd5f5b6ffa119642f24a5870321247dd
hanochshachar <goralot550@gmail.com>
2024-03-27 20:15:43 +0200

    start chaleng

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-let 
+const container = document.getElementById('container');
+const imagesArray = [{ src: '/images/instagram.svg', isDisplay: false, imageType: 1 },
+                    { src: '/images/instagram.svg', isDisplay: false, imageType: 1 },
+                    ]
